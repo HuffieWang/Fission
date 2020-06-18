7ca3dd5f9abe2639ac61bbec984433ae3bf174c1
Administrator <huffiewang@gmail.com>
2020-06-18 23:59:20 +0800

    UI/API

--- app/src/main/java/com/fission/MainPresenter.java
+++
@@ -1,6 +0,0 @@
-package com.fission;
-import com.musheng.android.common.mvp.BasePresenter;
-
-public class MainPresenter extends BasePresenter<MainContract.View> implements MainContract.Presenter {
-
-}
